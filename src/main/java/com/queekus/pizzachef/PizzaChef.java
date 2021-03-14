745d78acabb88410e2adbb03ac710a34ddf645dc
Annabelle Kennedy <queekusme@users.noreply.github.com>
2021-03-14 15:16:03 +0000

    Added mod names and cleaned unnecessary files and code

---
+++ src/main/java/com/queekus/pizzachef/PizzaChef.java
@@ -0,0 +1,14 @@
+package com.queekus.pizzachef;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(PizzaChef.MOD_ID)
+public class PizzaChef
+{
+    public static final String MOD_ID = "pizzachef";
+
+    public PizzaChef()
+    {
+        // TODO: Create Mod
+    }
+}
